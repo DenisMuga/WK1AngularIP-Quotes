be2799d8f1a7e22bd155d1bf4c4f7c8a16616949
DenisMuga <denismugah5@gmail.com>
2022-04-09 12:48:09 +0300

    feature: creating a class - quote

---
+++ src/app/quote.ts
@@ -0,0 +1,16 @@
+export class Quote {
+    showContent: boolean;
+    constructor(
+        public id: number,
+        public name: string,
+        public title: string,
+        public quote: string,
+        public author: string,
+        public datePosted: Date,
+        public likes: number,
+        public dislikes: number
+    ){
+        this.showContent=false;
+    }
+
+}
