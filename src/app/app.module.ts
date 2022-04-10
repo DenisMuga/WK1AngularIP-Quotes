fcade5ecbf82419fe4c4bd366a4b9e1b99d403d7
DenisMuga <denismugah5@gmail.com>
2022-04-10 10:37:27 +0300

    chore: importing DatePipe into app module ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { HighlightsDirective } from './highlights.directive';
+import { DatePipePipe } from './date-pipe.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuotesComponent,
-    HighlightsDirective
+    HighlightsDirective,
+    DatePipePipe
   ],
   imports: [
     BrowserModule,
